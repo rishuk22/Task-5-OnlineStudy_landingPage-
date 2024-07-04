079e9b6fcbbaf7cc249284a86051e10ecd9bec68
Rishu Kumari <142149799+rishuk22@users.noreply.github.com>
2024-07-05 00:11:30 +0530

    Add files via upload

---
+++ OnlineStudy/Src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import LandingPage from './components/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <LandingPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
